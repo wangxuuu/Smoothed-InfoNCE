59f589030c02aa02f237bba544e631066cd1158a
wangxuuu <wangxucoco@live.com>
2021-12-16 11:45:01 +0800

    update run

--- run.sh
+++ run.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 # conda activate conda
 
-python smoothed_infonce.py --n_epoch 6000 --alpha 0.001 --d 10
+python smoothed_infonce.py --n_epoch 10000 --alpha 0.001 --d 6
 
-python smoothed_infonce.py --n_epoch 6000 --alpha 0.01 --d 10
+python smoothed_infonce.py --n_epoch 10000 --alpha 0.01 --d 6
 
-python smoothed_infonce.py --n_epoch 6000 --alpha 0.1 --d 10
+python smoothed_infonce.py --n_epoch 10000 --alpha 0.1 --d 6
+
+python smoothed_infonce.py --n_epoch 10000 --alpha 0.001 --d 10
+
+python smoothed_infonce.py --n_epoch 10000 --alpha 0.01 --d 10
+
+python smoothed_infonce.py --n_epoch 10000 --alpha 0.1 --d 10
+
+python smoothed_infonce.py --n_epoch 10000 --alpha 0.001 --d 20
+
+python smoothed_infonce.py --n_epoch 10000 --alpha 0.01 --d 20
+
+python smoothed_infonce.py --n_epoch 10000 --alpha 0.1 --d 20
+
+python smoothed_infonce.py --n_epoch 10000 --alpha 0.001 --d 25
+
+python smoothed_infonce.py --n_epoch 10000 --alpha 0.01 --d 25
+
+python smoothed_infonce.py --n_epoch 10000 --alpha 0.1 --d 25
