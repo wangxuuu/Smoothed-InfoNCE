e265e48468a327a075c16eb84ba92d7594949f9b
wangxuuu <wangxucoco@live.com>
2022-02-06 18:09:22 +0800

    update

--- src/utils.py
+++ src/utils.py
@@ -30,13 +30,12 @@ def resample2(X, Y, batch_size, prob=None, replace=False):
     batch_Y = Y[index]
     return batch_X, batch_Y, index
 
-def plot_subfigure(net, X, Y, dimX, dimY, x0=None, y0=None, xmin=-5, xmax=5, ymin=-5, ymax=5, xgrids=50, ygrids=50, ax=None, show_details=True):
+def plot_subfigure(net, X, Y, dimX, dimY, x0=None, y0=None, xmin=-5, xmax=5, ymin=-5, ymax=5, xgrids=50, ygrids=50, ax=None, vmin=None, vmax=None, show_details=True):
     """
     The inputs should be X and Y, which are the coordinates of the points.
 
     net should be a neural network with Tensor inputs.
     """
-
     if  x0 == None:
         x0 = np.zeros((1, X.shape[1]))
     if y0 == None:
@@ -53,7 +52,7 @@ def plot_subfigure(net, X, Y, dimX, dimY, x0=None, y0=None, xmin=-5, xmax=5, ymi
     if ax is None:
         ax = plt.gca()
 
-    im = ax.pcolormesh(x, y, z, cmap="RdBu_r", shading="auto")
+    im = ax.pcolormesh(x, y, z, vmin=vmin, vmax=vmax, cmap="RdBu_r", shading="auto")
     # ax.figure.colorbar(im)
     if show_details:
         ax.figure.colorbar(im)
@@ -62,14 +61,41 @@ def plot_subfigure(net, X, Y, dimX, dimY, x0=None, y0=None, xmin=-5, xmax=5, ymi
                 title=r"Heatmap of $t(x,y)$")
     return im
 
-
-def plot_fig(net, X, Y, d=6):
+def plot_fig(net, X, Y, vmin=None, vmax=None, d=6):
     f, axs = plt.subplots(nrows=d,ncols=d,sharex=True, sharey=True)
     for i in range(d):
         for j in range(d):
-            im = plot_subfigure(net, X, Y, dimX=i, dimY=j, x0=None, y0=None, xmin=-5, xmax=5, ymin=-5, ymax=5, xgrids=50, ygrids=50, ax=axs[i, j], show_details=False)
+            im = plot_subfigure(net, X, Y, dimX=i, dimY=j, x0=None, y0=None, xmin=-5, xmax=5, ymin=-5, ymax=5, xgrids=50, ygrids=50, vmin=vmin, vmax=vmax, ax=axs[i, j], show_details=False)
     f.colorbar(im, ax=axs.ravel().tolist())
     return f
+
+def plot_subfigure_GT(net, X, Y, dimX, dimY, x0=None, y0=None, xmin=-5, xmax=5, ymin=-5, ymax=5, xgrids=50, ygrids=50, ax=None, show_details=True):
+    """
+    net: GT_logratio
+    """
+    if  x0 == None:
+        x0 = np.zeros((1, X.shape[1]))
+    if y0 == None:
+        y0 = np.zeros((1, Y.shape[1]))
+
+    x, y = np.mgrid[xmin:xmax:xgrids * 1j, ymin:ymax:ygrids * 1j]
+    with torch.no_grad():
+        z = (net(
+            torch.Tensor((np.arange(X.shape[-1]) == dimX).reshape(1, -1) *
+                    x.reshape(-1, 1) + x0).to(DEVICE),
+            torch.Tensor((np.arange(X.shape[-1]) == dimY).reshape(1, -1) *
+                    y.reshape(-1, 1) + y0).to(DEVICE),
+        ).reshape(x.shape).cpu())
+    if ax is None:
+        ax = plt.gca()
+    im = ax.pcolormesh(x, y, z, cmap="RdBu_r", shading="auto", vmax=20, vmin=-200)
+    if show_details:
+        ax.figure.colorbar(im) 
+        ax.set(xlabel="$x^{{({0})}}-x_0^{{({0})}}$",
+                ylabel="$x^{{({0})}}-x_0^{{({0})}}$",
+                title=r"Heatmap of $t(x,y)$")
+
+    return im
 
 def accuracy(true_labels,pred_labels):
     return sum((pred_labels > 0.5) == true_labels)/float(true_labels.shape[0])
